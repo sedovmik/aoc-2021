cd37dab54a4375c79bd8b1a7b6116dd89eb62d16
Mikhail Sedov <sedovmik@spotify.com>
2021-12-03 14:18:55 +0100

    Day03

---
+++ src/Day03.kt
@@ -0,0 +1,90 @@
+fun main() {
+
+    class Accumulator(size: Int) {
+        var total = 0
+        val counters = IntArray(size)
+
+        fun append(next: String) {
+            val array = next.toCharArray()
+            var idx = 0
+            for (ch in array) {
+                if (ch == '1') {
+                    counters[idx]++
+                }
+                idx += 1
+            }
+
+            total += 1
+        }
+
+        fun prod(): Int {
+            var gamma = 0
+            var epsilon = 0
+            for (idx in counters) {
+                if (2 * idx >= total) {
+                    gamma = 1 + (gamma shl 1)
+                    epsilon = epsilon shl 1
+                } else {
+                    gamma = gamma shl 1
+                    epsilon = 1 + (epsilon shl 1)
+                }
+            }
+
+            return gamma * epsilon
+        }
+    }
+
+    fun part1(input: Sequence<String>, size: Int): Int {
+        val acc = Accumulator(size)
+        for (s in input) {
+            acc.append(s)
+        }
+        return acc.prod()
+    }
+
+    fun part2(input: Sequence<String>, size: Int): Int {
+        fun walk(sorted: List<String>, size: Int, compare: (Int, Int) -> Boolean): Int {
+            var left = 0;
+            var right = sorted.size
+
+            for (i in 0 until size) {
+                val result = -1 - sorted.binarySearch(left, right)
+                    { str -> if (str[i] == '1') 1 else -1 }
+                if (compare(result - left, right - result)) {
+                    right = result
+                } else {
+                    left = result
+                }
+
+                if (left == right - 1) {
+                    break
+                }
+            }
+
+            return sorted[left].toInt(2)
+        }
+
+        val sorted = input.toList().sorted()
+
+        val oxygen = walk(sorted, size) { a, b -> a > b }
+        val co2 = walk(sorted, size) { a, b -> a <= b }
+
+        return oxygen * co2
+    }
+
+    readInput("Day03_test") {
+        check(part1(it, 5) == 198)
+    }
+
+    readInput("Day03") {
+        println(part1(it, 12))
+    }
+
+    readInput("Day03_test") {
+        check(part2(it, 5) == 230)
+    }
+
+    readInput("Day03") {
+        println(part2(it, 12))
+    }
+}
